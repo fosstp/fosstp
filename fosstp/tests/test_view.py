d71fdc2aafea8f5852662871026cef34e78d8920
William Wu <william@pylabs.org>
2015-12-29 15:58:45 +0800

    新增 unit tests for view

---
+++ fosstp/tests/test_view.py
@@ -0,0 +1,68 @@
+import unittest
+
+from pyramid import testing
+
+class ViewTests(unittest.TestCase):
+
+    def setUp(self):
+        self.request = testing.DummyRequest()
+        self.config = testing.setUp(request=self.request)
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_home_view(self):
+        from ..views import home_view
+        info = home_view(self.request)
+        self.assertFalse(info)
+
+    def test_news_view(self):
+        from ..views import news_view
+        info = news_view(self.request)
+        self.assertFalse(info)
+
+    def test_about_this_site_view(self):
+        from ..views import about_this_site_view
+        info = about_this_site_view(self.request)
+        self.assertFalse(info)
+
+    def test_workshop_view(self):
+        from ..views import workshop_view
+        info = workshop_view(self.request)
+        self.assertFalse(info)
+
+    def test_planet_view(self):
+        from ..views import planet_view
+        info = planet_view(self.request)
+        self.assertFalse(info)
+
+    def test_forum_view(self):
+        from ..views import forum_view
+        info = forum_view(self.request)
+        self.assertFalse(info)
+
+    def test_download_view(self):
+        from ..views import download_view
+        info = download_view(self.request)
+        self.assertFalse(info)
+
+    def test_link_view(self):
+        from ..views import link_view
+        info = link_view(self.request)
+        self.assertFalse(info)
+
+    def test_address_book_view(self):
+        from ..views import address_book_view
+        info = address_book_view(self.request)
+        self.assertFalse(info)
+
+    def test_log_view(self):
+        from ..views import log_view
+        info = log_view(self.request)
+        self.assertFalse(info)
+
+    def test_login_view(self):
+        from ..views import login_view
+        info = login_view(self.request)
+        self.assertFalse(info)
+
