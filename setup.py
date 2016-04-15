e252b285b9e25cf183a469cc8d8819c028fcfc55
William Wu <william@pylabs.org>
2016-04-15 22:33:14 +0800

    於 setup.py 指定版本並設定 dependency_links 以便直接安裝 mysql-connector-python

--- setup.py
+++ setup.py
@@ -9,27 +9,32 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
-    'pyramid',
-    'pyramid_jinja2',
-    'pyramid_debugtoolbar',
-    'pyramid_mailer',
-    'pyramid_wtforms',
-    'pyramid_tm',
-    'pyramid_sqlalchemy',
-    'pyramid_redis_sessions',
-    'alembic',
-    'deform',
-    'ColanderAlchemy',
-    'waitress',
+    'pyramid==1.6.1',
+    'pyramid_jinja2==2.6.2',
+    'pyramid_debugtoolbar==2.4.2',
+    'pyramid_mailer==0.14.1',
+    'pyramid_wtforms==2.4.0',
+    'pyramid_tm==0.12.1',
+    'pyramid_sqlalchemy==1.6',
+    'pyramid_redis_sessions==1.0.1',
+    'alembic==0.8.6',
+    'deform==2.0a2',
+    'ColanderAlchemy==0.3.3',
+    'mysql-connector-python==2.1.3',
+    'waitress==0.9.0b1',
     ]
 
 setup_requires = [
-    'pytest-runner',
+    'pytest-runner==2.7.1',
     ]
 
 test_requires = [
-    'pytest',
-    'webtest',
+    'pytest==2.9.1',
+    'WebTest==2.0.21',
+    ]
+
+dependency_links = [
+    'https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.3.tar.gz',
     ]
 
 setup(name='fosstp',
@@ -52,6 +57,7 @@ setup(name='fosstp',
       install_requires=requires,
       setup_requires=setup_requires,
       tests_require=test_requires,
+      dependency_links=dependency_links,
       test_suite="fosstp",
       entry_points="""\
       [paste.app_factory]
