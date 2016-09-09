547e19b96d380f3afa4d9931f9c5557b98ac9e0d
William Wu <william@pylabs.org>
2016-09-09 23:11:50 +0800

    更新相依套件版本，並將 mysql-connector-python 換成 pymysql

--- setup.py
+++ setup.py
@@ -8,19 +8,18 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
-    'pyramid==1.6.1',
+    'pyramid==1.7.3',
     'pyramid_jinja2==2.6.2',
-    'pyramid_debugtoolbar==2.4.2',
+    'pyramid_debugtoolbar==3.0.4',
     'pyramid_mailer==0.14.1',
     'pyramid_wtforms==2.4.0',
     'pyramid_tm==0.12.1',
     'pyramid_sqlalchemy==1.6',
     'pyramid_redis_sessions==1.0.1',
-    'alembic==0.8.6',
-    'deform==2.0a2',
-    'ColanderAlchemy==0.3.3',
-    'mysql-connector-python==2.1.3',
-    'waitress==0.9.0b1',
+    'alembic==0.8.7',
+    'PyMySQL==0.7.9',
+    'waitress==1.0.0',
+    'SQLAlchemy==1.0.15',
     ]
 
 setup_requires = [
@@ -31,10 +30,6 @@ test_requires = [
     'pytest',
     'WebTest',
     'pytest-cov',
-    ]
-
-dependency_links = [
-    'https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.3.tar.gz',
     ]
 
 setup(name='fosstp',
@@ -57,7 +52,6 @@ setup(name='fosstp',
       install_requires=requires,
       setup_requires=setup_requires,
       tests_require=test_requires,
-      dependency_links=dependency_links,
       test_suite="fosstp",
       entry_points="""\
       [paste.app_factory]
