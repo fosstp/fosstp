073ec8420b66385fd8d7aad17f4e4e83eec17a9c
William Wu <william@pylabs.org>
2016-11-24 08:29:56 +0800

    完成討論區基礎功能

--- fosstp/forms/forum.py
+++ fosstp/forms/forum.py
@@ -1,5 +1,5 @@
-from pyramid_wtforms import Form, StringField, TextAreaField
-from pyramid_wtforms.validators import InputRequired
+from pyramid_wtforms import Form, StringField, TextAreaField, HiddenField
+from pyramid_wtforms.validators import InputRequired, Regexp
 
 
 class ForumCategoryAddForm(Form):
@@ -9,3 +9,12 @@ class ForumCategoryAddForm(Form):
 class ForumTopicAddForm(Form):
     title = StringField('主題', [InputRequired('請輸入主題')])
     content = TextAreaField('內容', [InputRequired('請輸入內容')])
+
+class ForumTopicForm(ForumTopicAddForm):
+    id = HiddenField('id', [Regexp(r'\d+', message='需為正整數')])
+
+class ForumReplyAddForm(Form):
+    content = TextAreaField('內容', [InputRequired('請輸入內容')])
+
+class ForumReplyForm(ForumReplyAddForm):
+    id = HiddenField('id', [Regexp(r'\d+', message='需為正整數')])
--- fosstp/views/forum.py
+++ fosstp/views/forum.py
@@ -1,8 +1,8 @@
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 from pyramid_sqlalchemy import Session
-from ..models.forum import ForumCategoryModel, ForumTopicModel
-from ..forms.forum import ForumCategoryAddForm, ForumTopicAddForm
+from ..models.forum import ForumCategoryModel, ForumTopicModel, ForumReplyModel
+from ..forms.forum import ForumCategoryAddForm, ForumTopicAddForm, ForumTopicForm, ForumReplyAddForm, ForumReplyForm
 
 
 @view_config(route_name='forum', renderer='templates/forum.jinja2')
@@ -35,6 +35,73 @@ def forum_category_add_view_post(request):
         raise HTTPFound(location=request.route_path('forum'))
     else:
         return {'form': form}
+
+@view_config(route_name='forum_topic', renderer='templates/forum_topic.jinja2', request_method='GET', permission='member')
+def forum_topic_view_get(request):
+    forum_topic = Session.query(ForumTopicModel).get(int(request.matchdict['id']))
+    forum_reply_form = ForumReplyAddForm()
+    return {'forum_topic': forum_topic, 'forum_reply_form': forum_reply_form}
+
+@view_config(route_name='forum_topic', renderer='templates/forum_topic.jinja2', request_method='POST', permission='member')
+def forum_topic_view_post(request):
+    import transaction
+
+    forum_topic_id = int(request.matchdict['id'])
+    forum_reply_form = ForumReplyAddForm(request.POST)
+    if forum_reply_form.validate():
+        with transaction.manager:
+          forum_reply = ForumReplyModel()
+          forum_reply_form.populate_obj(forum_reply)
+          forum_reply.forum_topic_id = forum_topic_id
+          forum_reply.user_id = request.session['id']
+          Session.add(forum_reply)
+        raise HTTPFound(location=request.route_path('forum_topic', id=forum_topic_id))
+    else:
+      forum_topic = Session.query(ForumTopicModel).get(forum_topic_id)
+      return {'forum_topic': forum_topic}
+
+@view_config(route_name='forum_reply_edit', renderer='templates/forum_reply_edit.jinja2', request_method='GET', permission='member')
+def forum_reply_edit_view_get(request):
+    forum_reply = Session.query(ForumReplyModel).get(int(request.matchdict['id']))
+    form = ForumReplyForm(obj=forum_reply)
+    return {'form': form, 'forum_reply': forum_reply}
+
+@view_config(route_name='forum_reply_edit', renderer='templates/forum_reply_edit.jinja2', request_method='POST', permission='member')
+def forum_reply_edit_view_post(request):
+    import transaction
+
+    forum_reply_id = int(request.matchdict['id'])
+    forum_reply = Session.query(ForumReplyModel).get(forum_reply_id)
+    forum_topic_id = forum_reply.topic.id
+    form = ForumReplyForm(request.POST)
+    if form.validate():
+        with transaction.manager:
+            form.populate_obj(forum_reply)
+            Session.add(forum_reply)
+        raise HTTPFound(location=request.route_path('forum_topic', id=forum_topic_id))
+    else:
+        return {'form': form, 'forum_reply': forum_reply}
+
+@view_config(route_name='forum_topic_edit', renderer='templates/forum_topic_edit.jinja2', request_method='GET', permission='member')
+def forum_topic_edit_view_get(request):
+    forum_topic = Session.query(ForumTopicModel).get(int(request.matchdict['id']))
+    form = ForumTopicForm(obj=forum_topic)
+    return {'form': form, 'forum_topic': forum_topic}
+
+@view_config(route_name='forum_topic_edit', renderer='templates/forum_topic_edit.jinja2', request_method='POST', permission='member')
+def forum_topic_edit_view_post(request):
+    import transaction
+
+    forum_topic_id = int(request.matchdict['id'])
+    forum_topic = Session.query(ForumTopicModel).get(forum_topic_id)
+    form = ForumTopicForm(request.POST)
+    if form.validate():
+        with transaction.manager:
+            form.populate_obj(forum_topic)
+            Session.add(forum_topic)
+        raise HTTPFound(location=request.route_path('forum_topic', id=forum_topic_id))
+    else:
+        return {'form': form, 'forum_topic': forum_topic}
 
 @view_config(route_name='forum_topic_add', renderer='templates/forum_topic_add.jinja2', request_method='GET', permission='member')
 def forum_topic_add_view_get(request):
