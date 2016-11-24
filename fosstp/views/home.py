05ff5269ddf5c21190938805dc3249a19796d755
William Wu <william@pylabs.org>
2016-11-25 05:18:32 +0800

    完成首頁基本版型

--- fosstp/views/home.py
+++ fosstp/views/home.py
@@ -1,7 +1,11 @@
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPFound
+from pyramid_sqlalchemy import Session
+from ..models.news import NewsModel
+from ..models.forum import ForumTopicModel
 
 
 @view_config(route_name='home', renderer='templates/home.jinja2')
 def home_view(request):
-    return {}
+    news_list = Session.query(NewsModel).order_by(NewsModel.id.desc())[:10]
+    forum_topic_list = Session.query(ForumTopicModel).order_by(ForumTopicModel.id.desc())[:10]
+    return {'news_list': news_list, 'forum_topic_list': forum_topic_list}
