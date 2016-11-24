0efba5533e5b99ce3b3e0f1b5408edc313ff5f8f
William Wu <william@pylabs.org>
2016-11-25 06:31:09 +0800

    完成星球聯播顯示

--- fosstp/views/planet.py
+++ fosstp/views/planet.py
@@ -1,6 +1,9 @@
 from pyramid.view import view_config
+from pyramid_sqlalchemy import Session
+from ..models.planet import PlanetContentModel
 
 
 @view_config(route_name='planet', renderer='templates/planet.jinja2')
 def planet_view(request):
-    return {}
+    planet_contents = Session.query(PlanetContentModel).all()
+    return {'planet_contents': planet_contents}
